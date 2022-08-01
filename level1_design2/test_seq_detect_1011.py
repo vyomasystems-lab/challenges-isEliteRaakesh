7ae37e1ab4bb4e5e27a52ca687d69adc27cbd4b6
isEliteRaakesh <raakesh.ashwin@gmail.com>
2022-08-01 16:47:48 +0000

    updated files

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -9,10 +9,10 @@ from pathlib import Path
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
-
+"""
 @cocotb.test()
-async def test_seq_bug1(dut):
-    """Test for seq detection """
+async def test_seq_bug_1011_to_IDLE(dut):
+    #This is to detect the bug that occurs when 10111011 is passed
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
@@ -22,5 +22,79 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp=1011 model={1} dut={(dut.seq_seen.value)} ')
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp=1011 model={1} dut={(dut.seq_seen.value)} ')
+    assert dut.seq_seen.value == 1, "1011 detection round 2 failed"
+"""
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+@cocotb.test()
+async def test_seq_bug_101011(dut):
+    #This test is to detect the bug that occurs when 101011 is passed
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp=101011 model={1} dut={(dut.seq_seen.value)} ')
+    assert dut.seq_seen.value == 1, "1011 not detected from 101011 in overlap"
+
+
+"""
+@cocotb.test()
+async def test_seq_bug_11011(dut):
+    #This test is to detect the bug that occurs when 101011 is passed
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp=11011 model={1} dut={(dut.seq_seen.value)} ')
+    assert dut.seq_seen.value == 1, "1011 not detected from 11011 in overlap"
+"""
+ 
